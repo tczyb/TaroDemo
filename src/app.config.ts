15a546116e2ee6b3356f6ed21947c49008efd173
tczyb <407532646@qq.com>
2022-02-22 09:30:30 +0800

    Add files via upload

---
+++ src/app.config.ts
@@ -0,0 +1,13 @@
+import custom from './pages/custom.json';
+
+export default defineAppConfig({
+  pages: [
+    'pages/index/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  }
+})
